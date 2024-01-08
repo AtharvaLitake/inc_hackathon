cef244f174ad4fa7ff9dbdf7e5d8304e001134e2
AtharvaLitake <atharvalitake24@gmail.com>
2024-01-08 23:51:51 +0530

    bootstrap changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Problem from "./Components/Problem";
 import Guidelines from "./Components/Guidelines";
 import NavBar from "./Components/NavBar";
-import Register from "./Components/Register";
-import Contact from "./Components/Contact";
 import Register_Coming from "./Components/Register_Coming"
 import Timeline from "./Components/Timeline";
+import './App.css'
 function App() {
   return (
     <BrowserRouter>
