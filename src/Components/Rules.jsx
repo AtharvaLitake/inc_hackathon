94ef4eb46a6c06f8fdfc91379017fe23c8742b25
AtharvaLitake <atharvalitake24@gmail.com>
2024-01-09 01:30:44 +0530

    rules and guidelines

---
+++ src/Components/Rules.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import "../Styling/Rules.css";
+const Rules = () => {
+  return (
+    <div className="rules">
+      <h1>GUIDELINES</h1>
+      <div className="rule">
+        <h2>TEAM FORMATION</h2>
+        <ul>
+          <li>
+            All team members should be from same college; no inter-college teams
+            are allowed. However, members from different branches of the same
+            college/ institute are encouraged to form a team.
+          </li>
+          <li>
+            Each team would mandatorily comprise of minimum 4 & maximum of 6
+            members including the team leader..
+          </li>
+        </ul>
+      </div>
+      <div className="rule">
+        <h2>TEAM SUBMISSION PROCESS</h2>
+        <ul>
+          <li>
+            It is mandatory that the team leader enters the details for his/her
+            team.Team details once entered cannot be altered. The team leader
+            will be responsible for ensuring that all the data entered by him/
+            her is accurate.
+          </li>
+          <li>
+            The team’s name should be unique and must not contain the name of
+            your institute in any form.
+          </li>
+          <li>
+            Each team can submit multiple ideas. However, for each idea new
+            registration(including registration fees) is required.
+          </li>
+          <li>
+            The teams registered for TechFiesta shall be allowed to participate
+            in Impetus n Concepts 2024 other subevents (free of charges).
+            However, such teams must register for those events separately.
+          </li>
+        </ul>
+      </div>
+      <div className="rule">
+        <h2>IDEA NOMINATION PROCESS</h2>
+        <ul>
+          <li>
+            Idea submission form will start from 15/01/2024 and only 100 teams
+            will be shortlisted for further rounds.
+          </li>
+          <li>
+            Nominated Team’s team leader will get the notifications through mail
+            to complete the idea submission process.
+          </li>
+          <li>
+            <span className="imp">
+              The last date for team nomination and idea submission by team
+              leader on portal is till 15/02/2024 only. No request will be
+              entertained after the deadline.
+            </span>
+          </li>
+        </ul>
+      </div>
+      <div className="rule">
+        <h2>IDEA SELECTION CRITERIA</h2>
+        <ul>
+          <li>
+            Post Idea submission process, the ideas will be evaluated by experts
+            from industry. Evaluation criteria will include novelty of the idea,
+            complexity, clarity and details in the prescribed format,
+            feasibility, practicability, sustainability, scale of impact, user
+            experience and potential for future work progression.
+          </li>
+        </ul>
+      </div>
+      <div className="rule">
+        <h2>MODE OF HACKATHON</h2>
+        <ul>
+          <li>
+            TechFiesta PICT-Hackathon 2024’s pre-final rounds will be held
+            online. The tentative date for these rounds will be 23 rd to 29 th
+            Feb 2024. The final round will be held offline and will be held
+            between 15 th to 17 th March 2024.
+          </li>
+        </ul>
+      </div>
+      <div className="rule">
+        <h2>SHORTLISTED IDEAS FOR FINAL ROUND- ANNOUNCEMENT</h2>
+        <ul>
+          <li>
+            Notification about selected teams will be put up on our portal and
+            PICT website and will also be sent to their respective email IDs.
+          </li>
+          <li>
+            Be available for meetings, sessions and trainings during the
+            preparation phase; you will be notified about them in advance.
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Rules;
