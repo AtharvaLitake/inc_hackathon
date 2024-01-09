30ef84ade81561b2161911ef54f2d1b9cf078d09
AtharvaLitake <112816126+AtharvaLitake@users.noreply.github.com>
2024-01-09 13:22:44 +0530

    marquee

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,19 @@
+// Home.jsx
+
 import React from "react";
 import "../Styling/Home.css";
 import logo_hack from "../Assets/logo.png";
 import pict_logo from "../Assets/pict_logo.png";
+
 const Home = () => {
   return (
     <>
+      <div className="marquee">
+        <h4>
+          <i class="fa-solid fa-bell"></i>&nbsp;&nbsp;NOW PARTICIPATE AND WIN AMAZING
+          PRIZES WORTH 3.5 LAKH + &nbsp;&nbsp;<i class="fa-solid fa-bell"></i>
+        </h4>
+      </div>
       <div className="main_page">
         <div className="content-home">
           <div className="home_content">
@@ -16,7 +25,6 @@ const Home = () => {
               COMPUTER TECHNOLOGY PRESENTS
             </h2>
           </div>
-
         </div>
         <h1>TECHFIESTA - INTERNATIONAL PICT HACKATHON</h1>
         <img src={logo_hack} alt="logo_of hackathon" />
