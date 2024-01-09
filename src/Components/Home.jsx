fc3de77ad86673fdd62224885cb8d1ab7f312a5f
AtharvaLitake <112816126+AtharvaLitake@users.noreply.github.com>
2024-01-09 12:34:51 +0530

    home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import '../Styling/Home.css'
-import Footer from '../Components/Footer'
-import logo_hack from '../Assets/logo.png'
+import React from "react";
+import "../Styling/Home.css";
+import logo_hack from "../Assets/logo.png";
+import pict_logo from "../Assets/pict_logo.png";
 const Home = () => {
-    return (
-        <>
-        <div className='main_page'>
-            <div className="content-home">
-                <h2>SOCIETY FOR COMPUTER TECHNOLOGY AND RESEARCH'S
-                    PUNE INSTITUTE OF COMPUTER TECHNOLOGY
-                    PRESENTS
-                </h2>
-                <h1>
-                    TECHFIESTA - INTERNATIONAL PICT HACKATHON
-                </h1>
-                <img src={logo_hack} alt="logo_of hackathon" />
-            </div>/
+  return (
+    <>
+      <div className="main_page">
+        <div className="content-home">
+          <div className="home_content">
+            <img src={pict_logo} alt="logo_of pict" />
+          </div>
+          <div className="home_content">
+            <h2>
+              SOCIETY FOR COMPUTER TECHNOLOGY AND RESEARCH'S PUNE INSTITUTE OF
+              COMPUTER TECHNOLOGY PRESENTS
+            </h2>
+          </div>
+
         </div>
-        </>
-    );
-}
+        <h1>TECHFIESTA - INTERNATIONAL PICT HACKATHON</h1>
+        <img src={logo_hack} alt="logo_of hackathon" />
+      </div>
+    </>
+  );
+};
 
 export default Home;
