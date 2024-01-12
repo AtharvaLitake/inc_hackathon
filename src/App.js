69f78f7565f8390605691e0ebcf4bc19d4352f01
AtharvaLitake <112816126+AtharvaLitake@users.noreply.github.com>
2024-01-12 16:14:26 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import About from "./Components/About";
-import Problem from "./Components/Problem";
+import Problem_coming from "./Components/Problem_coming";
 import Guidelines from "./Components/Guidelines";
 import NavBar from "./Components/NavBar";
 import Register_Coming from "./Components/Register_Coming"
 import Timeline from "./Components/Timeline";
 import './App.css'
 import Footer from "./Components/Footer";
+import Register from "./Components/Register";
+import Submit from "./Components/Submit";
 function App() {
   return (
     <BrowserRouter>
@@ -17,10 +19,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/about" element={<About></About>}></Route>
-        <Route path="/problem" element={<Problem></Problem>}></Route>
+        <Route path="/problem" element={<Problem_coming></Problem_coming>}></Route>
         <Route path="/guidelines" element={<Guidelines></Guidelines>}></Route>
         <Route path="/register" element={<Register_Coming></Register_Coming>}></Route>
         <Route path="/timeline" element={<Timeline></Timeline>}></Route>
+        <Route path="/success" element={<Submit />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Components/Problem_coming.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../Styling/Register_Coming.css";
+const RegisterComing = () => {
+  return (
+    <div className="coming_soon">
+      <div className="coming_content">
+        <i class="fa-regular fa-clock"></i>
+        <h1>Problem Statements will be announced on 13th Jan 24</h1>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterComing;
