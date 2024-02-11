a3bfa1152b1ecc9fc680b0083555d27f667d1528
AtharvaLitake <112816126+AtharvaLitake@users.noreply.github.com>
2024-02-11 17:56:50 +0530

    home page change

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,19 +1,44 @@
-// Home.jsx
-
-import React from "react";
+import React, { useState } from "react";
 import "../Styling/Home.css";
 import logo_hack from "../Assets/logo.png";
 import pict_logo from "../Assets/pict_logo.png";
+import { Link } from "react-router-dom";
 
 const Home = () => {
+  const [showDialog, setShowDialog] = useState(false);
+
+  const handleDialogToggle = () => {
+    setShowDialog(!showDialog);
+  };
+
   return (
     <>
-
       <div className="marquee">
         <h4>
           REGISTRATION FEE - <b>&#8377; 400 /- </b>(Till 15th Feb)
         </h4>
       </div>
+      <div className="Registeration_extension" onClick={handleDialogToggle}>
+        <div className="extended">
+          <h2>
+            Registration <br />
+            Extended
+          </h2>
+        </div>
+      </div>
+      {showDialog && (
+        <div className="dialog">
+          <h1>Registeration Extended</h1>
+          <p>
+            In the view of the requests from different colleges, registeration
+            for hackathon is extended upto 15th Feb.
+          </p>
+          <Link to="/register" onClick={handleDialogToggle}>
+            Go to Next Page
+          </Link>
+          <button onClick={handleDialogToggle}>Close</button>
+        </div>
+      )}
       <div className="main_page">
         <div className="content-home">
           <div className="home_content">
